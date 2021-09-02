d22dee231a03e01a2ee27da3061a40630022774d
Renantx <66394069+Renantx@users.noreply.github.com>
2021-09-01 21:32:11 -0300

    Add files via upload

---
+++ Exibindo-numeros-pares.js
@@ -0,0 +1,21 @@
+//Exibindo Números Pares
+
+/* 
+Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
+ENTRADA
+
+Você receberá 1 valor inteiro N, onde N > 0.
+
+SAIDA
+
+Exiba todos os números pares até o valor de entrada, sendo um em cada linha. 
+*/
+
+let numero = gets();
+let par = 2;
+
+while (par <= numero) {
+    console.log(par);
+    par = par + 2;
+}
