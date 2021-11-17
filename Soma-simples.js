afa581eaec06a1ae017d8ada047570e2b65dffe5
Renantx <66394069+Renantx@users.noreply.github.com>
2021-11-17 17:41:48 -0300

    Add files via upload

---
+++ Soma-simples.js
@@ -0,0 +1,21 @@
+// Soma Simples
+
+/*
+Leia dois valores inteiros identificados como variáveis A e B. 
+Calcule a soma entre elas e chame essa variável de SOMA.
+
+A seguir escreva o valor desta variável.
+
+ENTRADA
+
+O arquivo de entrada contém 2 valores inteiros.
+
+SAIDA
+
+Imprima a variável SOMA com todas as letras maiúsculas, inserindo um espaço em branco antes e depois do símbolo de igualdade, seguido pelo valor correspondente à soma de A e B.
+*/
+
+let A = parseInt(gets());
+let B = parseInt(gets());
+let SOMA = A + B;
+print("SOMA = " + SOMA);
